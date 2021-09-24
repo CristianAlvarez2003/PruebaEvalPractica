65a8c803e0cba9b1311a5edcb9ff4f539a494998
CristianAlvarez2003 <cris0192776@gmail.com>
2021-09-24 05:33:04 -0600

    PRUEBAEvalPractica
    Prueba ListView

---
+++ app/src/main/java/com/example/pruebaevalpractica/DetallesRegistro.java
@@ -0,0 +1,10 @@
+package com.example.pruebaevalpractica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetallesRegistro extends AppCompatActivity {
+
+    private TextView tv_codigo, tv_descripcion, tv_precio;
+    private TextView tv_codigo1 , tv_descripcion1, tv_precio1, tv_fecha;
+
+}
